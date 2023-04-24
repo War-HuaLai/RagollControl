2b8f098a3a43c3ed89d987e2d52f0a867dec23b6
小左QAQ <lz1440918141@126.com>
2023-04-25 00:00:16 +0800

    V1.0

---
+++ Assets/Scripts/Active Ragdoll/Modules/InputModule.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ActiveRagdoll {
+    // Author: Sergio Abreu García | https://sergioabreu.me
+
+    /// <summary> Tells the ActiveRagdoll what it should do. Input can be external (like the
+    /// one from the player or from another script) and internal (kind of like sensors, such as
+    /// detecting if it's on floor). </summary>
+    public class InputModule : Module {
+        // ---------- EXTERNAL INPUT ----------
+
+        public delegate void onMoveDelegate(Vector2 movement);//委托
+        public onMoveDelegate OnMoveDelegates { get; set; }//委托
+        public void OnMove(InputValue value)//委托
+        {
+            OnMoveDelegates?.Invoke(value.Get<Vector2>());//委托
+        }
+
+        public delegate void onLeftArmDelegate(float armWeight);
+        public onLeftArmDelegate OnLeftArmDelegates { get; set; }
+        public void OnLeftArm(InputValue value) {
+            OnLeftArmDelegates?.Invoke(value.Get<float>());
+        }
+
+        public delegate void onRightArmDelegate(float armWeight);
+        public onRightArmDelegate OnRightArmDelegates { get; set; }
+        public void OnRightArm(InputValue value) {
+            OnRightArmDelegates?.Invoke(value.Get<float>());
+        }
+
+        // ---------- INTERNAL INPUT ----------
+
+        [Header("--- FLOOR ---")]
+        public float floorDetectionDistance = 0.3f;//检测距离
+        public float maxFloorSlope = 60;//最大斜坡角度
+
+        private bool _isOnFloor = true;//是否在地面上
+        public bool IsOnFloor { get { return _isOnFloor; } }//是否在地面上
+
+        Rigidbody _rightFoot, _leftFoot;
+
+
+        void Start() {
+            _rightFoot = _activeRagdoll.GetPhysicalBone(HumanBodyBones.RightFoot).GetComponent<Rigidbody>();//获取右脚
+            _leftFoot = _activeRagdoll.GetPhysicalBone(HumanBodyBones.LeftFoot).GetComponent<Rigidbody>();//获取左脚
+        }
+
+        void Update() {
+            UpdateOnFloor();
+        }
+
+        public delegate void onFloorChangedDelegate(bool onFloor);
+        public onFloorChangedDelegate OnFloorChangedDelegates { get; set; }
+        private void UpdateOnFloor() {
+            bool lastIsOnFloor = _isOnFloor;//上一帧是否在地面上
+
+            _isOnFloor = CheckRigidbodyOnFloor(_rightFoot, out Vector3 foo)
+                         || CheckRigidbodyOnFloor(_leftFoot, out foo);//检测是否在地面上
+
+            if (_isOnFloor != lastIsOnFloor)
+                OnFloorChangedDelegates(_isOnFloor);
+        }
+
+        /// <summary>
+        /// 检查刚体是否在地面上
+        /// </summary>
+        /// <param name="bodyPart">Part of the body to check</param
+        /// <returns> True if the Rigidbody is on floor </returns>
+        public bool CheckRigidbodyOnFloor(Rigidbody bodyPart, out Vector3 normal)//检查刚体是否在地面上
+        {
+            // Raycast
+            Ray ray = new Ray(bodyPart.position, Vector3.down);//向下发射射线
+            bool onFloor = Physics.Raycast(ray, out RaycastHit info, floorDetectionDistance, ~(1 << bodyPart.gameObject.layer));//检测是否在地面上
+
+            // Additional checks
+            onFloor = onFloor && Vector3.Angle(info.normal, Vector3.up) <= maxFloorSlope;
+
+            if (onFloor && info.collider.gameObject.TryGetComponent<Floor>(out Floor floor))//如果检测到地面
+                    onFloor = floor.isFloor;
+
+            normal = info.normal;
+            return onFloor;
+        }
+    }
+} // namespace ActiveRagdoll
---
+++ Assets/Scripts/Active Ragdoll/Others/Auxiliary.cs
@@ -0,0 +1,129 @@
+﻿#pragma warning disable 649
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ActiveRagdoll {
+    // Author: Sergio Abreu García | https://sergioabreu.me
+
+    [Serializable] public struct JointDriveConfig {
+        // Variables are exposed in the editor, but are kept readonly from code since
+        // changing them would have no effect until assigned to a JointDrive.
+        [SerializeField] private float _positionSpring, _positionDamper, _maximumForce;
+        public float PositionSpring { get { return _positionSpring; } }
+        public float PositionDamper { get { return _positionDamper; } }
+        public float MaximumForce { get { return _maximumForce; } }
+
+
+        public static explicit operator JointDrive(JointDriveConfig config) {
+            JointDrive jointDrive = new JointDrive {
+                positionSpring = config._positionSpring,
+                positionDamper = config._positionDamper,
+                maximumForce = config._maximumForce
+            };
+
+            return jointDrive;
+        }
+
+        public static explicit operator JointDriveConfig(JointDrive jointDrive) {
+            JointDriveConfig jointDriveConfig = new JointDriveConfig {
+                _positionSpring = jointDrive.positionSpring,
+                _positionDamper = jointDrive.positionDamper,
+                _maximumForce = jointDrive.maximumForce
+            };
+
+            return jointDriveConfig;
+        }
+
+        public readonly static JointDriveConfig ZERO = new JointDriveConfig
+                               { _positionSpring = 0, _positionDamper = 0, _maximumForce = 0};
+
+        public static JointDriveConfig operator *(JointDriveConfig config, float multiplier) {
+            return new JointDriveConfig {
+                _positionSpring = config.PositionSpring * multiplier,
+                _positionDamper = config.PositionDamper * multiplier,
+                _maximumForce = config.MaximumForce * multiplier
+            };
+        }
+    }
+
+    [Serializable]
+    public class BodyPart {
+        public string bodyPartName;
+
+        [SerializeField] private List<ConfigurableJoint> _joints;//关节
+        private List<JointDriveConfig> XjointDriveConfigs;//关节驱动
+        private List<JointDriveConfig> YZjointDriveConfigs;//关节驱动
+
+        [SerializeField] private float _strengthScale = 1;
+        public float StrengthScale { get { return _strengthScale; } }//力度
+        
+
+        public BodyPart(string name, List<ConfigurableJoint> joints) {
+            bodyPartName = name;
+            _joints = joints;
+        }
+
+        public void Init() {
+            XjointDriveConfigs = new List<JointDriveConfig>();
+            YZjointDriveConfigs = new List<JointDriveConfig>();
+
+            foreach (ConfigurableJoint joint in _joints) // For each joint in the body part
+            {
+                XjointDriveConfigs.Add((JointDriveConfig) joint.angularXDrive);
+                YZjointDriveConfigs.Add((JointDriveConfig) joint.angularYZDrive);
+            }
+
+            _strengthScale = 1;
+        }
+
+        public void SetStrengthScale(float scale) {
+            for (int i = 0; i < _joints.Count; i++) {
+                _joints[i].angularXDrive = (JointDrive) (XjointDriveConfigs[i] * scale);
+                _joints[i].angularYZDrive = (JointDrive) (YZjointDriveConfigs[i] * scale);
+            }
+
+            _strengthScale = scale;
+        }
+    }
+
+    [Serializable] public struct JointMotionsConfig//关节运动配置
+    {
+        public ConfigurableJointMotion angularXMotion, angularYMotion, angularZMotion;
+        public float angularXLimit, angularYLimit, angularZLimit;
+
+        public void ApplyTo(ref ConfigurableJoint joint) {
+            joint.angularXMotion = angularXMotion;
+            joint.angularYMotion = angularYMotion;
+            joint.angularZMotion = angularZMotion;
+
+            var softJointLimit = new SoftJointLimit();
+
+            softJointLimit.limit = angularXLimit / 2;
+            joint.highAngularXLimit = softJointLimit;
+
+            softJointLimit.limit = -softJointLimit.limit;
+            joint.lowAngularXLimit = softJointLimit;
+
+            softJointLimit.limit = angularYLimit;
+            joint.angularYLimit = softJointLimit;
+
+            softJointLimit.limit = angularZLimit;
+            joint.angularZLimit = softJointLimit;
+        }
+    }
+
+    public static class Auxiliary {
+        /// <summary>
+        /// Calculates the normalized projection of the Vector3 'vec'
+        /// onto the horizontal plane defined by the orthogonal vector (0, 1, 0)
+        /// </summary>
+        /// <param name="vec">The vector to project</param>
+        /// <returns>The normalized projection of 'vec' onto the horizontal plane</returns>
+        public static Vector3 GetFloorProjection(in Vector3 vec) {
+            return Vector3.ProjectOnPlane(vec, Vector3.up).normalized;
+        }
+    }
+
+}
